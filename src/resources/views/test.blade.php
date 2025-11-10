ecad038bc130ceef75d7700008959d78ed799739
Yuka-1992 <yuka.work.1992@gmail.com>
2025-11-10 16:11:33 +0900

    Fix: - ルートパスを/loginにリダイレクト - PageControllerと不要なビューを削除

--- src/resources/views/test.blade.php
+++
@@ -1,7 +0,0 @@
-@extends('layouts.app')
-
-@section('title', 'Test')
-
-@section('content')
-  <h1>Test</h1>
-@endsection
--- src/routes/web.php
+++ src/routes/web.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Http\Controllers\PageController;
 use App\Http\Controllers\UserAuthController;
 use App\Http\Controllers\AdminAuthController;
 use App\Http\Controllers\AttendanceController;
@@ -36,8 +35,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', [PageController::class, 'index']);
-Route::get('/test', [PageController::class, 'test']);
+Route::get('/', function () {
+    return redirect('/login');
+});
 Route::get('/register', [UserRegisterController::class, 'create'])->name('register.create');
 Route::post('/register', [UserRegisterController::class, 'store'])->name('register.store');
 Route::get('/login', [UserAuthController::class, 'create'])->name('login');
